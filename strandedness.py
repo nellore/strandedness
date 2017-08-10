e5d479b7fe0a8a04ee5e370980998026d070386c
JulianneDavid <juliannedavid@gmail.com>
2017-08-10 13:26:28 -0700

    Fixed strandedness prediction
    XS:A:* now is accurately interpreted as the strandedness of the gene relative to the global fwd/rev orientation, instead of strandedness of the read to the gene.

--- strandedness.py
+++ strandedness.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python2
+# coding=utf-8
 
 """
 Python 2.7 code for determining whether an RNA-seq experiment is stranded or
@@ -8,18 +9,16 @@ Required input:
     SAM file to check.
 
 Optional input:
-    Acceptable probability of correctness for strandness call.
+    Acceptable probability of correctness for strandedness call.
     Acceptable confidence of that probability.
 
 Limitations:
     Many!!
 
 Improvements to be made:
-    - Change to reservoir sampling instead of samtools?
-        ->  Take fastq file input and run through an aligner.
-            -> Take SRA accession numbers and download and run many in a row.
-
-    - FIX THE SAMPLE SIZE ESTIMATE
+    - Input = SRA accession numbers
+    - Start fastq-dump of SRA at one random spot.
+    - align each read as it's run- many in a row.
 
 """
 
@@ -69,25 +68,12 @@ if __name__ == '__main__':
     # arbitrary (?) alignment error rate
     error_rate = 0.2
 
-    # Calculate for error_rate, minimum number of reads to give confidence
-    #
-    # COME BACK TO THIS: confusion on std_dev stuff.
-    #       Standard deviation of read results, or of error?
-    #
-    print 'Checking for minimum number of required reads...'
-    num_errors = int(math.ceil(1000*error_rate))
-    sample_array = np.ones(1000)
-    sample_array[:num_errors] = 0
-    std_dev = np.std(sample_array)
-    z = stats.norm.ppf(1 - ((1-confidence)/2))
-    min_reads = int(math.ceil((z*std_dev / ((1-probability)/2)) ** 2))
-
-    # # ALternate for now:
     # min_reads = 7000000
-    # min_reads = 1600
+    min_reads = 1600
+    # min_reads = 700
 
     # collect min_reads reads from sam file: samtools -s?  reservoir sample?
-    print '\nSampling reads...'
+    print '\nSampling {} reads...'.format(min_reads)
     wc_output = subprocess.check_output(['wc', '-l', samfile])
 
     for s in wc_output.split():
@@ -99,15 +85,19 @@ if __name__ == '__main__':
     sample_seed = '{}'.format(add_seed)
     sampled_reads = subprocess.check_output(['samtools', 'view', '-s',
                                              sample_seed, samfile])
-    read_lines = sampled_reads.split('\n')
-    number_reads = len(read_lines)
-    read_lines = read_lines[0:number_reads-2]
+
+    cutoff = sampled_reads[:-2]
+    read_lines = cutoff.split('\n')
+    #
+    # read_lines = sampled_reads.split('\n')
+    # number_reads = len(read_lines)
+    # read_lines = read_lines[0:number_reads-2]
 
     # check number of sampled reads...
     print 'The length of the sam file is {} lines.'.format(int(sam_length))
     print 'The ratio should be {}.'.format(ratio)
     print 'The minimum number of reads should be {}.'.format(min_reads)
-    print 'The number of sampled reads is {}.'.format(number_reads-1)
+    print 'The number of sampled reads is {}.'.format(len(read_lines))
 
     # print "type of sampled_reads is {}.".format(type(sampled_reads))
     # print 'type of read_lines is {}.'.format(type(read_lines))
@@ -132,74 +122,136 @@ if __name__ == '__main__':
     entries = read.split()
     flag = int(entries[1])
     if flag & 1:
-        print (flag & 1)
-        paired += 1
+        paired = 1
     if paired:
         print 'The sample is paired-end.'
     else:
         print 'The sample is single end.'
 
-    # Reads:
-    # 	- Is it a primary alignment?  (position 2: not 256 in the bitmask
 
     print '\nChecking sampled reads: sense or antisense?'
     sense = 0
     antisense = 0
     checked_reads = 0
-    if not paired:
-        for read in read_lines:
-            # Check for primary alignment?  Not done yet....
+    total_reads = 0
+    print_num = 2000
+
+
+    for read in read_lines:
+        entries = read.split()
+        total_reads += 1
+        flag = int(entries[1])
+        if not flag & 256:
             check_sense = re.findall('XS:A:[+-]', read.upper())
             if not check_sense:
                 continue
-            elif check_sense[0] == 'XS:A:+':
-                sense += 1
-                checked_reads += 1
-            elif check_sense[0] == 'XS:A:-':
-                antisense += 1
-                checked_reads += 1
+            elif not paired:
+                if check_sense[0] == 'XS:A:+' and flag & 16:
+                    sense += 1
+                    checked_reads += 1
+                    if total_reads < print_num:
+                        print '+ and rev comp, {} reads'.format(checked_reads)
+                elif check_sense[0] == 'XS:A:+' and not flag & 16:
+                    antisense += 1
+                    checked_reads += 1
+                    if total_reads < print_num:
+                        print '+ and forward, {} reads'.format(checked_reads)
+                elif check_sense[0] == 'XS:A:-' and flag & 16:
+                    antisense += 1
+                    checked_reads += 1
+                    if total_reads < print_num:
+                        print '- and rev comp, {} reads'.format(checked_reads)
+                elif check_sense[0] == 'XS:A:-' and not flag & 16:
+                    sense += 1
+                    checked_reads += 1
+                    if total_reads < print_num:
+                        print '- and forward, {} reads'.format(checked_reads)
+                else:
+                    continue
             else:
-                continue
-    else:
-        for read in read_lines:
-            entries = read.split()
-            flag = int(entries[1])
-            check_sense = re.findall('XS:A:[+-]', read.upper())
-            if not check_sense:
-                continue
-            elif check_sense[0] == 'XS:A:+':
-                if flag & 64:
-                    # indicates first read; arbitrarily call these forward.
-                    sense += 1
-                    checked_reads += 1
-                elif flag & 128:
-                    antisense += 1
-                    checked_reads += 1
-                else:
-                    continue
-            elif check_sense[0] == 'XS:A:-':
-                if flag & 64:
-                    # indicates first read; arbitrarily = main direction.
-                    antisense += 1
-                    checked_reads += 1
-                elif flag & 128:
-                    sense += 1
-                    checked_reads += 1
+                if check_sense[0] == 'XS:A:+' and flag & 16:
+                    if flag & 64:
+                        sense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} first rev comp +'.format(total_reads)
+                    elif flag & 128:
+                        antisense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} second rev comp +'.format(total_reads)
+                    else:
+                        continue
+                elif check_sense[0] == 'XS:A:+' and not flag & 16:
+                    if flag & 64:
+                        antisense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} first forward +'.format(total_reads)
+                    elif flag & 128:
+                        sense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} second forward +'.format(total_reads)
+                    else:
+                        continue
+                elif check_sense[0] == 'XS:A:-' and flag & 16:
+                    if flag & 64:
+                        antisense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} first rev comp -'.format(total_reads)
+                    elif flag & 128:
+                        sense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} second rev comp -'.format(total_reads)
+                    else:
+                        continue
+                elif check_sense[0] == 'XS:A:-' and not flag & 16:
+                    if flag & 64:
+                        sense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} first forward -'.format(total_reads)
+                    elif flag & 128:
+                        antisense += 1
+                        checked_reads += 1
+                        if total_reads < print_num:
+                            print '{} second forward -'.format(total_reads)
+                    else:
+                        continue
                 else:
                     continue
 
-    print 'sense number is {}'.format(sense)
-    print 'antisense number is {}'.format(antisense)
+    print 'number of sense reads is {}'.format(sense)
+    print 'number of antisense reads is {}'.format(antisense)
     print 'total number of checked reads is {}'.format(checked_reads)
+    r = error_rate * 0.5
+    # Now we have min_reads processed, X as sense and Y as antisense.
+    # Check probability:
+    errors = min(sense, antisense)
+    stranded_prob = stats.binom.pmf(errors, checked_reads, r)
 
-    # # Now we have min_reads processed, X as sense and Y as antisense.
-    # # Check probability:
-    # stranded_errors = min(sense, antisense)
-    # current_prob = stats.binom.pmf(stranded_errors, checked_reads, error_rate)
+    r = 0.5
+    unstranded_prob = stats.binom.pmf(errors, checked_reads, r)
+
+
+    print 'number of "stranded errors" is {}'.format(errors)
+    print 'probability that the read is stranded is {}'.format(stranded_prob)
+
+    unstranded_errors = int(abs(sense-antisense)/2)
+    print 'number of unstranded errors is {}'.format(unstranded_errors)
+    print 'probability that the read is unstranded is {}'.format(unstranded_prob)
+
+
     #
     # mean = stranded_errors/checked_reads
-    # std = np.std(sense_count)
-    # scaled_std = np.std(sense_count)
+    # std = np.std(checked_reads)
+    #
+    # scaled_std = std/
     #
     # # Calculate confidence interval for current sample.
-    # conf_int = stats.norm.interval(0.95,loc=1,scale=std/math.sqrt(len(s)))
+    # # conf_int = stats.norm.interval(confidence,loc=1,scale=std/math.sqrt(len(s)))
+    # conf_int = stats.norm.interval(confidence,loc=mean,scale=std/math.sqrt(len(s)))
+    #

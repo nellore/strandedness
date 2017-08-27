a7cf34f7367a851d157888cb381e052ebeb75f67
JulianneDavid <juliannedavid@gmail.com>
2017-08-26 17:00:01 -0700

    Improves B-H correction and file writing
    Previous version stopped at the lowest k for which P_(k+1) > (k+1)*alpha/m, instead of (correctly) stopping at the highest k for which P_k <= k*alpha/m.  This version also eliminates extra parentheses in the written SRA_pvals file.

--- strandedness.py
+++ strandedness.py
@@ -19,9 +19,7 @@ Improvements to be made:
     - How many reads are required?  Unknown.
 """
 
-# import what needs to be imported.
 import argparse
-import numpy as np
 import os
 import random
 import re
@@ -66,9 +64,11 @@ if __name__ == '__main__':
     # Currently set low to check for correct running.
     required_reads = 50
     pval_list = []
-    sra_array = np.genfromtxt(sra_file, delimiter=',', dtype=None)
+    sra_array = open(sra_file, 'r')
     SRA_num = 0
+
     for experiment in sra_array:
+        experiment = experiment.split(',')
         start_time = time.time()
         if experiment[0][0] == 'S':
             sra_acc = str(experiment[0])
@@ -80,6 +80,7 @@ if __name__ == '__main__':
         if SRA_num >= 5:
             break
         SRA_num += 1
+
         reads_path = os.path.join(out_path, '{}_reads.sam'.format(sra_acc))
         reads_file = open(reads_path, 'w')
 
@@ -177,24 +178,38 @@ if __name__ == '__main__':
         print '\nmoving on to the next SRA accession number \n'
 
         pval_list.append((sra_acc, p_value))
-        pval_file.write('{}, {}\n'.format(sra_acc, p_value))
+        pval_file.write('{},{}\n'.format(sra_acc, p_value))
         reads_file.close()
         end_time = time.time()
         print "elapsed time for this SRA was {}".format(end_time-start_time)
 
     pval_file.close()
+    sra_array.close()
 
     # Benjamini-Hochberg multiple-testing correction
     # Also available in a separate script, BH_correction.py
-    pval_list.sort(key=lambda line: line[1])
+    pval_list.sort(key=lambda line: line[1], reverse=True)
     m = len(pval_list)
-    stranded_expts = []
-    stranded_file = open('stranded_SRAs.txt', 'w')
-    for k, expt in enumerate(pval_list, 1):
-        if expt[1] <= alpha * k / m:
-            stranded_expts.append(expt)
-            print '{} expt: stranded with p value {}.'.format(expt[0], expt[1])
-            stranded_file.write('{}\n'.format(expt))
-        else:
-            print 'BH correction complete!  Stranded list is finished.'
+    discovery = 0
+    for k_rev, expt in enumerate(pval_list, 1):
+        k = m - k_rev + 1
+        p_value = float(expt[1])
+        print 'checking line (k is) {}'.format(k)
+        print 'check is {}'.format(alpha * k / m)
+        print 'p value is {}'.format(p_value)
+        if p_value <= alpha * k / m:
+            discovery = 1
+            largest_k = k
+            print 'largest k is {}'.format(k)
             break
+    if discovery:
+        stranded_expts = list(reversed(pval_list))[:k]
+        print 'BH correction complete!  Stranded experiments are these:'
+        print stranded_expts
+
+        stranded_file_path = os.path.join(out_path, 'stranded_SRAs.txt')
+        with open(stranded_file_path, 'w') as stranded_file:
+            for set in stranded_expts:
+                stranded_file.write('{},{}\n'.format(set[0], set[1]))
+    else:
+        print 'BH correction complete! All experiments were unstranded.'

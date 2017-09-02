e17676acee947dcf084d27b6e482855e30c2c37a
JulianneDavid <juliannedavid@gmail.com>
2017-09-02 16:25:42 -0700

    writes alignments to file; removes -k hisat option

--- strandedness.py
+++ strandedness.py
@@ -20,12 +20,19 @@ Improvements to be made:
 """
 
 import argparse
+from itertools import groupby
 import os
 import random
 import re
 import subprocess as sp
 from scipy import stats
 import time
+import tempfile
+import errno
+import atexit
+import shutil
+import gzip
+
 
 def extract_sra_data(csv_line):
     '''Returns required data for one SRA experiment.
@@ -78,127 +85,52 @@ def get_hisat_input(required, multiplier, total, fastq_path, acc, output,
     and the list of random spots (to be saved for later reference).
     '''
     dl_time_start = time.time()
-    # # # 10,000 reads straight in a row.
-    # spot_path = os.path.join(output, '{}_spots.txt'.format(acc))
-    # start_spot = random.randint(1, total-11000)
-    # spot_file = open(spot_path, 'w')
-    # spot_file.write('{}\n'.format(start_spot))
-    # spot = str(start_spot)
-    # stopspot = str(start_spot + 9999)
-    # # If we have x unique spots, we will get <x reads out, since the
-    # # -E quality filter rejects some reads before downloading them.
-    # fastq = sp.check_output(['{}'.format(fastq_path), '-I', '-B',
-    #                          '-W', '-E', '--split-spot',
-    #                          '--skip-technical', '-N', spot, '-X',
-    #                          stopspot, '-Z', acc])
-    # lines = fastq.split('\n')
-    # format_lines = []
-    # read_format = []
-    # for i, line in enumerate(lines, 1):
-    #     if i % 2 == 0:
-    #         format_lines += [line]
-    #     if pairedtag:
-    #         if i % 8 == 0:
-    #             read_format.extend(['\t'.join(format_lines) + '\n'])
-    #             format_lines = []
-    #         if (i-1) % 8 == 0:
-    #             format_lines += [line]
-    #     else:
-    #         if i % 4 == 0:
-    #             read_format.extend(['\t'.join(format_lines) + '\n'])
-    #             format_lines = []
-    #         if (i-1) % 4 == 0:
-    #             format_lines += [line]
-    # read_input = ''.join(read_format)
-    # hisat_formatted_input = read_input
-    # spot_file.close()
-    # dl_time_stop = time.time()
-    # print 'download time was {}'.format(dl_time_stop - dl_time_start)
-    # return hisat_formatted_input
-
-    # # X bins, 10000/X from each
-    required_spots = required * multiplier
     spot_path = os.path.join(output, '{}_spots.txt'.format(acc))
-    spot_file = open(spot_path, 'w')
-    num_bins = 50
-    bin_spots = required_spots/num_bins
-    bin_size = total/num_bins
-    bin = 1
-    # bin_start = 1
-    # bin_stop = bin_size
-    read_format = []
-    while bin <= num_bins:
-        binstart = time.time()
-        bin_start = (bin - 1) * bin_size + 1
-        bin_stop = bin * bin_size
-        start_spot = random.randint(bin_start, bin_stop - bin_spots)
-        spot_file.write('{}\n'.format(start_spot))
-        spot = str(start_spot)
-        stop_spot = str(start_spot + bin_spots)
-        # If we have x unique spots, we will get <x reads out, since the
-        # -E quality filter rejects some reads before downloading them.
-        fastq = sp.check_output(['{}'.format(fastq_path), '-I', '-B',
-                                 '-W', '-E', '--split-spot',
-                                 '--skip-technical', '-N', spot, '-X',
-                                 stop_spot, '-Z', acc])
-        lines = fastq.split('\n')
-        format_lines = []
-        for i, line in enumerate(lines, 1):
-            if i % 2 == 0:
-                format_lines += [line]
-            if pairedtag:
-                if i % 8 == 0:
-                    read_format.extend(['\t'.join(format_lines) + '\n'])
-                    format_lines = []
-                if (i-1) % 8 == 0:
+    with open(spot_path, 'w') as spot_file:
+        required_spots = required * multiplier
+        num_bins = 100
+        bin_spots = required_spots/num_bins
+        bin_size = total/num_bins
+        bin = 1
+        read_format = []
+        while bin <= num_bins:
+            bin_start = (bin - 1) * bin_size + 1
+            bin_stop = bin * bin_size
+            start_spot = random.randint(bin_start, bin_stop - bin_spots)
+            spot_file.write('{}\n'.format(start_spot))
+            spot = str(start_spot)
+            stop_spot = str(start_spot + bin_spots)
+            fastq = sp.check_output(['{}'.format(fastq_path), '-I', '-B',
+                                     '-W', '-E', '--split-spot',
+                                     '--skip-technical', '-N', spot, '-X',
+                                     stop_spot, '-Z', acc])
+            lines = fastq.split('\n')
+            format_lines = []
+            for i, line in enumerate(lines, 1):
+                if i % 2 == 0:
                     format_lines += [line]
-            else:
-                if i % 4 == 0:
-                    read_format.extend(['\t'.join(format_lines) + '\n'])
-                    format_lines = []
-                if (i-1) % 4 == 0:
-                    format_lines += [line]
-        # bin_start += bin_size
-        # bin_stop += bin_stop
-        bin += 1
-        binend = time.time()
-        print 'bintime = {}'.format(binend-binstart)
-    
-    read_input = ''.join(read_format)
-    hisat_formatted_input = read_input
-    spot_file.close()
-    dl_time_stop = time.time()
-    print 'download time was {}'.format(dl_time_stop - dl_time_start)
-    return hisat_formatted_input
-
-#     # one spot at a time
-#     required_spots = required * multiplier
-#     spot_list = random.sample(range(total-1), required_spots)
-#     spot_list.sort()
-#     spot_path = os.path.join(output, '{}_spots.txt'.format(acc))
-#     spot_file = open(spot_path, 'w')
-#     hisat_formatted_input = ''
-#     for start_spot in spot_list:
-#         spot_file.write('{}\n'.format(start_spot))
-#         spot = str(start_spot)
-#         # If we have x unique spots, we will get <x reads out, since the
-#         # -E quality filter rejects some reads before downloading them.
-#         fastq = sp.check_output(['{}'.format(fastq_path), '-I', '-B',
-#                                  '-W', '--fasta', '--split-spot',
-#                                  '--skip-technical', '-N', spot, '-X',
-#                                  spot, '-Z', acc])
-#         # did --fasta instead of -E quality filtering
-#         lines = fastq.split('\n')
-#         format_lines = lines[:1] + lines[1::2]
-#         read_input = '\t'.join(format_lines) + '\n'
-#         hisat_formatted_input += read_input
-#     spot_file.close()
-#     dl_time_stop = time.time()
-#     print 'download time was {}'.format(dl_time_stop - dl_time_start)
-#     return hisat_formatted_input
+                if pairedtag:
+                    if i % 8 == 0:
+                        read_format.extend(['\t'.join(format_lines) + '\n'])
+                        format_lines = []
+                    if (i-1) % 8 == 0:
+                        format_lines += [line]
+                else:
+                    if i % 4 == 0:
+                        read_format.extend(['\t'.join(format_lines) + '\n'])
+                        format_lines = []
+                    if (i-1) % 4 == 0:
+                        format_lines += [line]
+            bin += 1
+        read_input = ''.join(read_format)
+        hisat_formatted_input = read_input
+        dl_time_stop = time.time()
+        print 'download time was {}'.format(dl_time_stop - dl_time_start)
+        return hisat_formatted_input
 
 
-def align_sampled_reads(hisat2_path, reference_genome, reads):
+def align_sampled_reads(hisat2_path, reference_genome, outpath, acc, reads,
+                        temp_dir=None):
     '''Returns SAM format reads aligned by hisat2.
 
     Input:
@@ -208,22 +140,33 @@ def align_sampled_reads(hisat2_path, reference_genome, reads):
     reads: fastq reads correctly formatted for the hisat2 -12 option (string,
         one read per line tab separated, name seq qual if single-end or
         name seq qual seq qual if paired-end)
+    temp_dir: dir in which to put temporary fastq
 
     Returns a list of aligned reads in SAM format.
     '''
-    align_time_start = time.time()
-    align_command = ('{h2} -k 1 --no-head --12 - -x {ref}'
-                     ).format(h2=hisat2_path, ref=reference_genome)
-    align_process = sp.Popen(align_command, stdin=sp.PIPE, stdout=sp.PIPE,
-                             shell=True, executable='/bin/bash')
-    align_output = align_process.communicate(input=reads)
-    alignment = align_output[0].split('\n')[:-1]
-    align_time_stop = time.time()
-    print 'alignment time was {}'.format(align_time_stop - align_time_start)
-    return alignment
+    align_start = time.time()
+    # alignment = []
+    fastq = os.path.join(temp_dir, 'temp.fq')
+    reads_path = os.path.join(outpath, '{}_reads.sam.gz'.format(acc))
+    with open(fastq, 'w') as fastq_file:
+        fastq_file.write(reads)
+    align_command = ('set -exo pipefail; cat {fastq} | {h2} --no-head '
+                     '--12 - -x {ref} | gzip > {file}').format(fastq=fastq,
+                     h2=hisat2_path, ref=reference_genome, file=reads_path)
+    try:
+        sp.check_call(align_command, shell=True, executable='/bin/bash')
+    except sp.CalledProcessException:
+        # For now, it's going to raise the exception, but we want to be
+        # robust to bad SRA runs, so you might want to arrange it so
+        # an accession number is skipped if this happens
+        raise sp.CalledProcessError
+
+    align_end = time.time()
+    print '\ntime for alignment was {}\n'.format(align_end - align_start)
+    return reads_path
 
 
-def read_is_useful(previous_read, SAM_flag, has_XS_A_tag):
+def read_is_useful(SAM_flag, has_XS_A_tag):
     '''Checks requirements for usability of the current read.
 
     Input
@@ -239,11 +182,9 @@ def read_is_useful(previous_read, SAM_flag, has_XS_A_tag):
     Returns True if the read is useful, otherwise returns False.
     '''
     secondary_read = SAM_flag & 256
-    paired = SAM_flag & 1
     if has_XS_A_tag:
         if not secondary_read:
-            if not (paired and previous_read):
-                return True
+            return True
     return False
 
 
@@ -290,6 +231,9 @@ if __name__ == '__main__':
                         'multiplier for generating the number of reads to '
                         'download from fastq-dump, to account for quality '
                         'filtering and for not all reads being useful.')
+    parser.add_argument('--temp-dir', '-t', type=str, default=None,
+                        help='where to put temporary files; default is '
+                             'securely created directory')
 
     args = parser.parse_args()
     alpha = args.alpha
@@ -301,65 +245,64 @@ if __name__ == '__main__':
     required_reads = args.requiredreads
     read_multiplier = args.multiplier
 
-    pval_file_path = os.path.join(out_path, 'SRA_pvals.txt')
-    pval_file = open(pval_file_path, 'w')
-
-    pval_list = []
-    sra_array = open(sra_file, 'r')
+    # Create temporary directory for storing fastq
+    if args.temp_dir is not None:
+        try:
+            os.makedirs(args.temp_dir)
+        except OSError as e:
+            if e.errno != errno.EEXIST:
+                raise
+    temp_dir = tempfile.mkdtemp(dir=args.temp_dir)
+    atexit.register(shutil.rmtree, temp_dir)
+    
+    read_shuffle_seed = 1
     SRA_num = 0
+    pv_path = os.path.join(out_path, 'SRA_pvals.txt')
+    with open(sra_file, 'r') as sra_array, open(pv_path, 'w', 0) as pval_file:
+        sra_array.next()
+        for experiment in sra_array:
+            # For a sample run: with a large .csv file, this will check X SRAs.
+            if SRA_num >= 3:
+                break
+            SRA_num += 1
 
+            hisat_input = get_hisat_input(required_reads, read_multiplier,
+                                          num_spots, fastq_dump, sra_acc,
+                                          out_path, paired)
+            reads_path = align_sampled_reads(hisat2, ref_genome, out_path,
+                                             sra_acc, hisat_input, temp_dir)
+            try:
+                reads_path = align_sampled_reads(hisat2, ref_genome, out_path,
+                                                sra_acc, hisat_input, temp_dir)
+            except sp.CalledProcessError:
+                continue
 
-    # sra_sample = random.sample(range(2499), 30)
+            sense = 0
+            checked_reads = 0
+            random.seed(read_shuffle_seed)
+            with gzip.open('{}'.format(reads_path), 'r') as aligned_reads:
+                sort_by_names = lambda x: x.split('\t')[0]
+                for key, alignments in groupby(aligned_reads, sort_by_names):
+                    random.shuffle(list(alignments))
+                    for entry in alignments:
+                        read = entry.split()
+                        flag = int(read[1])
+                        XS_A_tag = re.findall('XS:A:[+-]', entry)
+                        if read_is_useful(flag, XS_A_tag):
+                            sense += read_sense(flag, XS_A_tag)
+                            checked_reads += 1
+                            break
 
-    for i, experiment in enumerate(sra_array):
-        if i == 0:
-            continue
-            
-        sra_acc, num_spots, paired = extract_sra_data(experiment)
+            antisense = checked_reads - sense
+            r = 0.5
+            # 2-sided & symmetrical: same result whether we pick sense or
+            # antisense
+            p_value = stats.binom_test(sense, checked_reads, r)
+            print '\nthe SRA accession number is {}'.format(sra_acc)
+            print 'number of sense reads is {}'.format(sense)
+            print 'number of antisense reads is {}'.format(antisense)
+            print 'total number of junction reads is {}'.format(checked_reads)
+            print 'The unstranded p-value is {}'.format(p_value)
+            print '\nmoving on to the next SRA accession number \n'
 
-        # For a sample run: with a large .csv file, this will check X SRAs.
-        if SRA_num >= 3:
-            break
-        SRA_num += 1
-
-        reads_path = os.path.join(out_path, '{}_reads.sam'.format(sra_acc))
-        reads_file = open(reads_path, 'w', 1)
-        hisat_input = get_hisat_input(required_reads, read_multiplier,
-                                      num_spots, fastq_dump, sra_acc, out_path,
-                                      paired)
-        aligned_reads = align_sampled_reads(hisat2, ref_genome, hisat_input)
-
-        sense = 0
-        checked_reads = 0
-        useful = True
-        last_read = not useful
-
-        for entry in aligned_reads:
-            read = entry.split()
-            flag = int(read[1])
-            XS_A_tag = re.findall('XS:A:[+-]', entry)
-            if read_is_useful(last_read, flag, XS_A_tag):
-                sense += read_sense(flag, XS_A_tag)
-                checked_reads += 1
-                last_read = useful
-                reads_file.write('{}\n'.format(entry))
-            else:
-                last_read = not useful
-
-        antisense = checked_reads - sense
-        r = 0.5
-        # 2-sided & symmetrical: same result whether we pick sense or antisense
-        p_value = stats.binom_test(sense, checked_reads, r)
-        print '\nthe SRA accession number is {}'.format(sra_acc)
-        print 'number of sense reads is {}'.format(sense)
-        print 'number of antisense reads is {}'.format(antisense)
-        print 'total number of junction reads is {}'.format(checked_reads)
-        print 'The unstranded p-value is {}'.format(p_value)
-        print '\nmoving on to the next SRA accession number \n'
-
-        pval_list.append([sra_acc, p_value])
-        pval_file.write('{},{}\n'.format(sra_acc, p_value))
-        reads_file.close()
-
-    pval_file.close()
-    sra_array.close()
+            pval_file.write('{},{}\n'.format(sra_acc, p_value))
